299e4ec026f7ad4d737a261e048c39667e912b7b
Lidianys <lidianysrodriguez96@gmail.com>
2022-01-03 11:24:19 -0300

    perfeccionamiento de css, creacion de las otras dos paginas de html, error al linkear las paginas, commit para que dani pueda actualizar su pagina

---
+++ src/data/RickyMorty/RickyMorty.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'RickyMorty',
+  items: [
+    { id: 'img1', image: 'img1.jpn', bgColor: '#339933' },
+    { id: 'img2', image: 'img2.jpn', bgColor: '#339933' },
+    { id: 'img3', image: 'img3.jpn', bgColor: '#339933' },
+    { id: 'img4', image: 'img4.jpn', bgColor: '#F05032' },
+    { id: 'img5', image: 'img5.jpn', bgColor: '#F05032' },
+    { id: 'img6', image: 'img6.jpn', bgColor: '#F05032' },
+    { id: 'img7', image: 'img7.jpn', bgColor: '#1572B6' },
+    { id: 'img8', image: 'img8.jpn', bgColor: '#1572B6' },
+    { id: 'img9', image: 'img9.jpn', bgColor: '#1572B6' },
+  ],
+};
