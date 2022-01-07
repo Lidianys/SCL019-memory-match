0945783dce561b1b0d9ccd84813e7eedb0ffc0e1
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-01-07 12:42:10 -0300

    Se muestran las imagenes en el HTML con manipulación dinámica del DOM

--- src/data/RickyMorty/RickyMorty.js
+++ src/data/RickyMorty/RickyMorty.js
@@ -1,14 +1,14 @@
 export default {
   name: 'RickyMorty',
   items: [
-    { id: 'img1', image: 'https://indiehoy.com/wp-content/uploads/2020/07/rick-morty.jpg'},
-    { id: 'img2', image: 'https://i.pinimg.com/originals/53/e7/3d/53e73d572e470f38dbb3b9e1333b17eb.jpg'},
-    { id: 'img3', image: 'img3.jpg'},
-    { id: 'img4', image: 'img4.jpg'},
-    { id: 'img5', image: 'img5.jpg'},
-    { id: 'img6', image: 'img6.jpg'},
-    { id: 'img7', image: 'img7.jpg'},
-    { id: 'img8', image: 'img8.jpg'},
-    { id: 'img9', image: 'img9.jpg'},
+    { id: 'img1', image: 'data/RickyMorty/img1.jpg'},
+    { id: 'img2', image: 'data/RickyMorty/img2.jpg'},
+    { id: 'img3', image: 'data/RickyMorty/img3.jpg'},
+    { id: 'img4', image: 'data/RickyMorty/img4.jpg'},
+    { id: 'img5', image: 'data/RickyMorty/img5.jpg'},
+    { id: 'img6', image: 'data/RickyMorty/img6.jpg'},
+    { id: 'img7', image: 'data/RickyMorty/img7.jpg'},
+    { id: 'img8', image: 'data/RickyMorty/img8.jpg'},
+    { id: 'img9', image: 'data/RickyMorty/img9.jpg'},
   ],
 };
