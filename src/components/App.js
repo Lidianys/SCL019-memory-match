53b45fc2d6393f205d72dae18290b86307bd371d
Lidianys <lidianysrodriguez96@gmail.com>
2022-01-07 21:01:07 -0300

    se implemento el dom, salen las imagenes de back and front.

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,64 @@
 //
  import RickyMorty from '../data/RickyMorty/RickyMorty.js';
  //console.log(RickyMorty);
-let elementsItems= RickyMorty.items;
-//let cards;
-//let image = document.createElement('prueba');
-//let frontFace = document.getElementById("front-face") ;
-let image=0; 
-const cardsContainer=document.getElementById('cardsContainer');
+ 
+let elementsItems= RickyMorty.items;//definimos los elementos de la dat
+let image=0; //definir imagen frontal de la carta
+let backImage; //definir imagen back de las cartas
+const allCards = document.querySelectorAll(".memory-game");
 
-for(let i = 0; i < elementsItems.length; i ++){
- // console.log(elementsItems[i].image);
-  image = elementsItems[i].image;
-  const newDiv = document.createElement('div');
-  const elemento = document.createElement('img');
-  const contenido = document.createTextNode(elemento.src= image);
-  elemento.appendChild(contenido);
-  newDiv.appendChild(elemento);
-  document.getElementById('cardsContainer').appendChild(newDiv);
-  console.log(elemento);
+for(let i = 1; i < elementsItems.length; i ++){
+  image = elementsItems[i].image;//almacena la src de las imagenes front
+  backImage = elementsItems[0].image;// almacena la src de las imagenes back
+  const newDiv = document.createElement('div');//crea una etiqueta div
+  const frontFace = document.createElement('img');// crea etiqueta img para imagenes front
+  const backFace = document.createElement("img");//crea etiqueta img para imagenes back
+  const frontCard = document.createTextNode(frontFace.src= image);// contiene las imagenes de front
+  const backCard = document.createTextNode(backFace.src= backImage);//contiene la imagen de back
+  frontFace.appendChild(frontCard);//pone las imagenes dentro de la etiqueta img de front
+  backFace.appendChild(backCard);//pone la imagen dentro de la etiqueta img de back
+  newDiv.appendChild(frontFace);//se pone la etiqueta img dentro del div
+  newDiv.appendChild(backFace);//se pone la etiqueta img dentro del div
+  document.getElementById('cardsContainer').appendChild(newDiv);//se crea el div dentro del select
+  console.log(frontFace);
+  newDiv.id = "memory-game";
+  frontFace.className = "cards";
+  backFace.className= "cards";
+  //backCard.className = "frontFace";
+
 }
+function flipCard() {
+  console.log("estoy clickeando");
+} 
 
 
-const App = () => {
+
+allCards.forEach(card => card.addEventListener ("click", flipCard ));
+
+/*const App = (RickyMorty) => {
+  let elementsItems= RickyMorty.items;//definimos los elementos de la dat
+  let image=0; //definir imagen frontal de la carta
+  let backImage; //definir imagen back de las cartas
+  
+  for(let i = 1; i < elementsItems.length; i ++){
+    image = elementsItems[i].image;//almacena la src de las imagenes front
+    backImage = elementsItems[0].image;// almacena la src de las imagenes back
+    const newDiv = document.createElement('div');//crea una etiqueta div
+    const frontFace = document.createElement('img');// crea etiqueta img para imagenes front
+    const backFace = document.createElement("img");//crea etiqueta img para imagenes back
+    const frontCard = document.createTextNode(frontFace.src= image);// contiene las imagenes de front
+    const backCard = document.createTextNode(backFace.src= backImage);//contiene la imagen de back
+    frontFace.appendChild(frontCard);//pone las imagenes dentro de la etiqueta img de front
+    backFace.appendChild(backCard);//pone la imagen dentro de la etiqueta img de back
+    newDiv.appendChild(frontFace);//se pone la etiqueta img dentro del div
+    newDiv.appendChild(backFace);//se pone la etiqueta img dentro del div
+    document.getElementById('cardsContainer').appendChild(newDiv);//se crea el div dentro del select
+    console.log(frontFace);
+
+
+
+
+
   //const el = document.createElement('div');
   //const elemento = document.createElement('img');
   //const contenido = document.createTextNode(elementsItems[i].image);
@@ -35,12 +72,14 @@ const App = () => {
   //el.className = 'App';
   //el.textContent = 'Hola mundo!';
 
-  //return elemento;
+  return newDiv;
 };
-export default App;
+}*/
+
+//export default App;
 
 
- //esta funciona `<img src="${elementsItems[i].image}" alt="imagenes">`;
+ /*esta funciona `<img src="${elementsItems[i].image}" alt="imagenes">`;
  
  //cards = elements[i].image;
  //document.getElementById("prueba").innerHTML= cards;
@@ -54,6 +93,6 @@ const App = () => {
   //el.textContent = 'Hola mundo!';
 
   return elemento;
-};
+};*/
 
-//export default App;*/
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,66 @@
-import App from './components/App.js';
+//import App from './components/App.js';
+import RickyMorty from '../data/RickyMorty/RickyMorty.js';
 
-//ent.getElementById('root').appendChild(App());
+
+
+let elementsItems= RickyMorty.items;//definimos los elementos de la dat
+let image=0; //definir imagen frontal de la carta
+let arrayCards = new Array();
+//let backImage; //definir imagen back de las cartas
+//const cards = document.querySelectorAll(".cards");
+
+
+for(let i = 1; i < elementsItems.length; i ++){
+  image = elementsItems[i].image;//almacena la src de las imagenes front
+  arrayCards = arrayCards[i] + image;
+ // 
+  //let arrayCardsB = arrayCards.concat(arrayCards);
+ // backImage = elementsItems[0].image;// almacena la src de las imagenes back
+  const newDiv = document.createElement('div');//crea una etiqueta div
+  const frontFace = document.createElement('img');// crea etiqueta img para imagenes front
+  //const backFace = document.createElement("img");//crea etiqueta img para imagenes back
+  const frontCard = document.createTextNode(frontFace.src= image);// contiene las imagenes de front
+  //const backCard = document.createTextNode(backFace.src= backImage);//contiene la imagen de back
+  frontFace.appendChild(frontCard);//pone las imagenes dentro de la etiqueta img de front
+ // backFace.appendChild(backCard);//pone la imagen dentro de la etiqueta img de back
+  newDiv.appendChild(frontFace);//se pone la etiqueta img dentro del div
+ // newDiv.appendChild(backFace);//se pone la etiqueta img dentro del div
+  frontFace.className = "cards";
+ // backFace.className= "cards";
+  const dupCards = newDiv.cloneNode(true);
+  //newDiv = newDiv.conc
+  document.getElementById('cardsContainer').appendChild(newDiv);//se crea el div dentro del select
+  document.getElementById('cardsContainer').appendChild(dupCards);//se crea el div dentro del select
+  //console.log(frontFace);
+  newDiv.className = "memory-game";
+  dupCards.className= "memory-game";
+  console.log("hola" + arrayCards);
+}
+
+//console.log("hola" + arrayCards);
+/*function duplicateElements(array, times) {
+    return array.reduce((res, current) => {
+        return res.concat(Array(times).fill(current));
+    }, []);
+   
+  }
+ /* duplicateElements([arrayCards], 5);
+  for(let i = 1; i < arrayCards.length; i ++){
+   
+  }*/
+ 
+/*function flipCard() {
+  console.log("estoy clickeando");
+} 
+
+cards.forEach(card => card.addEventListener ("click", flipCard ));*/
+
+
+//console.log("hpla" + cards);
+
+
+
+
+
+
+//document.getElementById('root').appendChild(App());
