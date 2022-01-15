cdb32c927a95fddd2794409733882b71c722cb95
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-01-14 22:57:38 -0300

    Reestructuracion de App, cambio de shuffel, variables

--- src/components/App.js
+++ src/components/App.js
@@ -2,107 +2,47 @@
 import RickyMorty from '../data/RickyMorty/RickyMorty.js';
 export default App;
 
-
-
- let elementsItems= RickyMorty.items;//definimos los elementos de la data
-//---funcion Fisher-Yates Shuffle ----
-function shuffle() {
-  let currentIndex = elementsItems.length; 
-  let randomIndex =0;
-  let tempValue=0;
-  
-  while(currentIndex !== 0){
-    randomIndex=Math.floor(Math.random()*currentIndex);
-    currentIndex-=1;
-    tempValue= elementsItems[currentIndex];
-    elementsItems[currentIndex]=elementsItems[randomIndex];
-    elementsItems[randomIndex]=tempValue;
-    console.log("retern de shuffle" + elementsItems);
-  }
+//*******desordenamos la data ************/
+const board = document.querySelector(".board");
+const randomize = () =>{
+  let elementsItems= RickyMorty.items;
+  elementsItems.sort (()=> Math.random()-0.5);
   return elementsItems;
-  
-}
-console.log("retern de shuffle" + elementsItems);
+};
+
+/**********FUNCION APP****************/
 function App (){
-  let image = new Array(); //definir imagen frontal de la carta
-  let idCard = new Array();
- // let backImage=0; //definir imagen back de las cartas 
-  
- shuffle();
-  for(let i = 0; i < elementsItems.length; i ++){
-    image= elementsItems[i].image;//almacena la src de las imagenes front
-    //elementsItems[i].addEventListener("click", displayCard);
-    idCard = elementsItems[i].id;
-   //---- crea una etiqueta div y se le asigna una class-------------------
-   console.log("imagenes" + image);
-   console.log("id" + idCard);
+  const dataCard = randomize();
+  for(let i = 0; i < dataCard.length; i ++){
+    const card = document.createElement("div"); //creamos la tarjeta
+    const front = document.createElement("img");
+    const back = document.createElement("div");
+    card.className = "card";
+    front.className = "front";
+    back.className ="back";
+    //adjuntamos un id a la 
+    front.id=dataCard[i].id;
+    //adjuntamos la imagen a la tarjeta
+    front.src=dataCard[i].image;
+    //adjuntamos las cartas a la section 
+    board.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
 
-    const newDiv = document.createElement("div");
-    newDiv.className= "memory-card";
-    newDiv.onclick =  flipCard; //-----------------funciono------------
+    card.addEventListener("click",(e)=>{
+      //let contadorClick = 0; 
+      card.classList.toggle("toggleCard"); 
+      /*if (contadorClick <= 2){ *** prueba de contador de click
+        contadorClick = contadorClick + 1; 
+        console.log("Hola Dani contadorClick", contadorClick);
+      }*/
+      
+      checkCards(e);
+    });
+  }; 
+};
 
-   //---- crea etiquetas img para imagenes front y back / se le asinga una clase -- 
+function checkCards (e){
+  const clickCard= e.target;
 
-    const frontFace = document.createElement("img");
-    const backFace = document.createElement("img");
-    frontFace.className = "front-face";
-    backFace.className= "back-face";
-   //---- Se le asigna src a cada img de front y back   
-    const frontCard = document.createTextNode(frontFace.src= image);
-    const backCard = document.createTextNode(backFace.src="data/RickyMorty/fondocards.png");
-   //---- se inserta a cada nodo img su respectiva imagen -------  
-    frontFace.appendChild(frontCard);
-    backFace.appendChild(backCard);
-   //---- se inserta a cada nodo div su respectiva img -------  
-    newDiv.appendChild(frontFace);
-    newDiv.appendChild(backFace);
-   //---- se inserta al nodo Select los div ------- 
-   let prueba = document.getElementById("memory-game");
-   console.log("eprueba" , prueba);
-    document.getElementById("memory-game").appendChild(newDiv);
-    //console.log(frontFace);   
-    //backCard.className = "front-face";
-    
-    console.log("elementrs items" + elementsItems);
-    //para dar el click
-  }
-} 
-  console.log("estoy dando clcik");
-
-function flipCard(e){
-  let cards= document.getElementsByClassName("memory-card");
-  //cards.classList.remove("toggleCard");
-  //cards.target.style = "flip";
-  //e.target.style.background="red"; //------------funciona--------
-  e.target.className= "memory-card";
-  //e.target.className("memory-card.flip").style = "180deg";
-  //e.target.style.flip="180deg";
-  console.log(e.target.style);
-  console.log(e.target);
-
-  //return cards;
-}
-  /*let flipCard = function (e){
-    console.log("evento" , e.target.style);
-    let cards = document.getElementsByClassName("memory-card");
-    //console.log("hola", cards.target.style);
-    cards.classList.add("toggleCard");
-    //e.target.style.classList.toggle('toggleCard');
-   // e.target.style =  cards.className.add("toggleCard");
-    //this.classList.toggle("open");
-    //this.classList.toggle("show");
-    //this.classList.toggle("disabled");
-    //this.classList.add('flip');
-    console.log("click");
-    alert("hola");
-  }
-
-  
-  function flipCard(){
-   console.log("Hola");
-   }
-}
-//para checkear las cartas que seleccionan<<<<<<<<<<<<<<<<<<
-//const checkCards = (e) => { 
-  //const clickedCard = e.target;
-  //console.log(clickedCard);*/
+};
