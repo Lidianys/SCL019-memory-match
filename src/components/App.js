399e4d899c43bc1e67513c4ba8f9db408f169502
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-01-18 21:55:55 -0300

    Se agrego el reset

--- src/components/App.js
+++ src/components/App.js
@@ -64,47 +64,58 @@ function checkCards (e){
      card.classList.remove("flipped");
      card.style.pointerEvents= "none";
      points++;
-     console.log("puntos +" + points/2);
      puntuacion.textContent = points/2;
      //aqui va el pop up de ganador
      
     });
    }
    else {
-     console.log("wrong");
+    
      flippedCards.forEach(card =>{
        card.classList.remove("flipped");
        setTimeout(() =>
          card.classList.remove("toggleCard"),1000);
-         if(flippedCards.length === 0)  {
+        /* if(flippedCards.length === 0)  {
            alert("perdistes"); //aqui se llama a la funcion restaurar
-         } 
+         } */
      });
    }
    if(toggleCard.length === 12){
-    //alert("ganastes");
+     //alert("ganastes");
+     const button = document.querySelector("button"); 
       const popup = document.querySelector(".popup-wrapper");
       const close = document.querySelector(".popup-close");
-       popup.style.display ="block";
+      popup.style.display ="block";
+      button.addEventListener("click",()=> {
+        console.log("entramos al botón");
+        popup.style.display = "none";
+        restart();
+      });
+     
        close.addEventListener("click",()=>{
          popup.style.display = "none";
        } );
+      
+    }
   }
- }
  
-}
-//restart
-/*const restart = () =>{
-  let cardData = randomize();
-  let faces = document.querySelectorAll(".face");
-  let cards = document.querySelectorAll(".card");
-  Selection.style.pointerEvents= "none";
-  cardData.forEach(item, index) => {
-    cards[index].classList.remove("toggleCard");
-    //randomize
-    setTimeout(() => {
+
+  //Se reinicia todo!
+  const restart = () =>{
+    console.log("Entraste a RESTART");
+    let cardData = randomize();
+    let front = document.querySelectorAll(".front");
+    let card = document.querySelectorAll(".card");
+    points=0;
+    moves=0;
+     puntuacion.textContent = points;
+      movimientos.textContent = moves;
+    cardData.forEach((item, index) => {
+      card[index].classList.remove("toggleCard");
+      card[index].style.pointerEvents= "all";
+      front[index].src=item.image;
+      card[index].setAttribute ("name",item.id);
       
     });
   }
-}*/
-
+}
