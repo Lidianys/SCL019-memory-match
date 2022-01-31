3e31971b90afe2d984469ac2feec021ac21aba37
DanielaAlcalaDaboin <daniela.alcalad@gmail.com>
2022-01-31 11:38:08 -0300

    actualizacion

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import AppR from './components/AppR.js';
 //Se llama la función crea el tablero y las cartas
-document.getElementById("container").appendChild(AppR());
+document.getElementById("board").appendChild(AppR());
